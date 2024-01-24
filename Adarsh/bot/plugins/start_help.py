472cb980c6844eabcb48200bc1ffd6857587c071
SPYDERx007 <89435036+SPYDERx007@users.noreply.github.com>
2024-01-24 14:40:43 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -128,8 +128,8 @@ async def help_handler(bot, message):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("💁‍♂️ Owner", url="https://github.com/Adarsh-Goel")],
-                [InlineKeyboardButton("💥 Source Code", url="https://github.com/Greymattersbot/Filestreambot-pro")]
+                [InlineKeyboardButton("💁‍♂️ Owner", url="https://t.me/ItsBruceOfficial")],
+                [InlineKeyboardButton("Made With ❤️ By", url="https://t.me/ItsBruceOfficial")]
             ]
         )
     )
